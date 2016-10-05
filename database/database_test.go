810284f068acc7eac3f73d8d5d81ef57f896116f
Paris Goudas <paris@darkstar.gr>
2016-10-05 23:51:00 +0100

    Add mock for logbook and database test

--- database/database_test.go
+++ database/database_test.go
@@ -1,12 +1,27 @@
 package database
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/superdecimal/dolce/logbook"
+)
 
 const dbName string = "TestDB"
 
 func TestNew(t *testing.T) {
-	database, err := New(dbName)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	dLog := logbook.NewMockLogbook(ctrl)
+	dLog.EXPECT().GetAll()
+
+	database, err := New(dLog, dbName)
+
 	if err != nil {
 		t.Error("Expected database, got ", database)
+	} else {
+		fmt.Println("Success")
 	}
 }
